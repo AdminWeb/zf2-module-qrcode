7f1830bc0746df006f2f469abad71525d815fd0c
IgorDePaula <principe.borodin@gmail.com>
2013-05-15 08:41:10 -0300

    Add instructions and phpdocblocks

--- README.md
+++ README.md
@@ -4,3 +4,21 @@ zf2-module-qrcode
 A zend framework 2 module for generate qr code using the google api.
 
 See more in https://developers.google.com/chart/infographics/docs/qr_codes
+
+#### Usage:
+
+1. In the controller
+
+   ```php
+	$qr = $this->getServiceLocator()->get('QRCode');
+        $qr->isHttps(); // or $qr->isHttp();
+        $qr->setData('Lorem Ipsum');
+        $qr->setDimensions(50, 50);
+        return new ViewModel(array('img'=> $qr->getParameters()));
+    ```
+
+2. In the view
+
+   ```php
+	<img src="<?php echo $this->img; ?>" />
+    ```
--- src/QRCode/Service/QRCode.php
+++ src/QRCode/Service/QRCode.php
@@ -1,13 +1,26 @@
 <?php
 
 namespace QRCode\Service;
-
+/**
+ * QRCode service for Zend Framework 2
+ */
 class QRCode {
 
+    /**
+     * Properties for the qrcode
+     * @var Array
+     */
     private $properties = array();
-    private $https = false;
+    
+    /**
+     * The Final Endpoint
+     * @var String The final endpoint 
+     */
     private $endpoint = null;
     
+    /**
+     * The Start endpoint
+     */
     const END_POINT = 'chart.googleapis.com/chart?';
 
     function __construct() {
@@ -16,27 +29,51 @@ class QRCode {
         $this->setTypeChart();
     }
     
+    /**
+     * Is Http?
+     */
     public function isHttp()
     {
         $this->endpoint = 'http://'.self::END_POINT;
     }
+    /**
+     * Is Https?
+     */
     public function isHttps()
     {
         $this->endpoint = 'https://'.self::END_POINT;
     }
 
+    /**
+     * Set chart type, here 'qr' is default chart type, mainly for qrcode 
+     * @param String $chart Chart Type
+     */
     public function setTypeChart($chart = 'qr') {
         $this->properties['cht'] = $chart;
     }
 
+    /**
+     * Returns the chart type
+     * @return String 
+     */
     public function getTypeChart() {
         return $this->properties['cht'];
     }
 
-    public function getParameters() {
+    /**
+     * Get the link for image of qrcode
+     * @return String
+     */
+    public function getResult() {
         return $this->endpoint.http_build_query($this->properties);
     }
 
+    /**
+     * Set dimensions (width, height) of image
+     * @param Integer $w Width of image
+     * @param Integer $h Height of image
+     * @throws \InvalidArgumentException
+     */
     public function setDimensions($w, $h) {
         if (is_int($w) && is_int($h)) {
             $this->properties['chs'] = "{$w}x{$h}";
@@ -45,34 +82,63 @@ class QRCode {
         }
     }
 
+    /**
+     * Return the dimensions of image
+     * @return String
+     */
     public function getDimensions() {
         return $this->properties['chs'];
     }
 
+    /**
+     * Set the charset of content data. Default is 'UTF-8'
+     * @param String $charset charset of content data
+     */
     public function setCharset($charset = 'UTF-8') {
         $this->properties['choe'] = $charset;
     }
 
+    /**
+     * Return the charset of content data
+     * @return String
+     */
     public function getCharset() {
         return $this->properties['choe'];
     }
     
+    /**
+     * Set level of loss of content and margin of image
+     * @param String $cl Level of loss
+     * @param Integer $m Margin of image
+     */
     public function setCorrectionLevel($cl = 'L',$m = 0)
     {
         $this->properties['chld'] = "{$cl}|{$m}";
     }
+    
+    /**
+     * Return level of loss of content and margin of image
+     * @return String
+     */
     public function getCorrectionLevel()
     {
         return $this->properties['chld'] ;
     }
     
+    /**
+     * Set content data in urlencode format
+     * @param String $data Content
+     */
     public function setData($data)
     {
         $this->properties['chl'] = urlencode($data);
     }
+    /**
+     * Return the content data in urldecode format.
+     * @return String
+     */
     public function getData()
     {
-        return $this->properties['chl'];
+        return urldecode($this->properties['chl']);
     }
-
 }
